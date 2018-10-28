26c066aba8043a26987f7ae1b1bbfaf2d1780122
everthis <wwwaap@gmail.com>
2018-10-28 22:06:27 +0800

    fix convert error.

--- ffmpeg/convert.js
+++ ffmpeg/convert.js
@@ -2,11 +2,12 @@ const ffmpeg = require("fluent-ffmpeg");
 const path = require("path");
 
 function convertFn(fpath) {
-  return new ffmpeg(path)
+  const extname = path.extname(fpath);
+  return new ffmpeg(fpath)
     .videoBitrate("2500k")
     .videoCodec("libx264")
     .size("?x720")
-    .audioCodec("libfaac")
+    .audioCodec("libmp3lame")
     .audioBitrate("128k")
     .on("error", function(err) {
       console.log("An error occurred: " + err.message);
@@ -15,7 +16,10 @@ function convertFn(fpath) {
       console.log("Processing finished !");
     })
     .save(
-      `/home/everthis/projects/pt-task/public/downloads/${path.basename(fpath)}`
+      `/home/everthis/projects/pt-task/public/downloads/${path.basename(
+        fpath,
+        extname
+      )}.mp4`
     );
 }
 module.exports = { convertFn };
