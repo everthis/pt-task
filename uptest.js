0ae315cb1faee70baf0833703b109e6cdc7423f7
everthis <wwwaap@gmail.com>
2018-11-19 22:53:55 +0800

    mod upload test script

--- uptest.js
+++ uptest.js
@@ -2,10 +2,7 @@ require("dotenv").config();
 const OSS = require("ali-oss");
 const path = require("path");
 const fs = require("fs");
-const bull = require("bull");
-const { REDIS_HOST_PORT } = process.env;
-const uploadQueue = new bull("upload", REDIS_HOST_PORT);
-const setTaskLog = require("../util/setTaskLog");
+const setTaskLog = require("./util/setTaskLog");
 const ENV = process.env;
 const client = new OSS({
   region: ENV.ALI_OSS_REGION,
@@ -15,29 +12,70 @@ const client = new OSS({
   secure: true
 });
 
-async function progress(percent, checkpoint, res) {
-  console.log("precent:", percent);
-  console.log("checkpoint:", checkpoint);
-}
-async function multipartUpload(fpath) {
-  const fileName = path.basename(fpath);
-  try {
-    let result = await client.multipartUpload(fileName, fpath, {
-      partSize: 10 * 1024 * 1024,
-      progress,
-      meta: {}
-    });
-    console.log(result);
-    let head = await client.head("object-name");
-    console.log(head);
-  } catch (e) {
-    // 捕获超时异常
-    if (e.code === "ConnectionTimeoutError") {
-      console.log("Woops,超时啦!");
-      // do ConnectionTimeoutError operation
+function exec(job) {
+  const { hash, fpath } = job;
+  let checkpoint = undefined;
+  const startTs = new Date().getTime();
+  return new Promise(async (resolve, reject) => {
+    const fileName = path.basename(fpath);
+    // retry 5 times
+    for (let i = 0; i < 5; i++) {
+      try {
+        const result = await client.multipartUpload(fileName, fpath, {
+          checkpoint,
+          async progress(percentage, cpt) {
+            await setUploadProgress({
+              hash,
+              fileName,
+              start_ts: startTs,
+              end_ts: new Date().getTime(),
+              percentage: percentage * 100
+            });
+            console.log(percentage);
+            checkpoint = cpt;
+          }
+        });
+        await setUploadProgress({
+          hash,
+          fileName,
+          start_ts: startTs,
+          end_ts: new Date().getTime(),
+          percentage: 100
+        });
+        fs.unlinkSync(fpath);
+        await setTaskLog({
+          hash,
+          step: "removeConvertedFile",
+          log: {
+            fileName,
+            progress: 100
+          }
+        });
+        resolve("");
+        console.log(result);
+        break; // break if success
+      } catch (e) {
+        console.log("--------upload-error-----------", e);
+      }
     }
-    console.log(e);
-  }
+    reject("");
+  });
 }
 
-multipartUpload("");
+function setUploadProgress({ hash, percentage, fileName, start_ts, end_ts }) {
+  return setTaskLog({
+    hash,
+    step: "upload",
+    log: {
+      fileName,
+      start_ts,
+      end_ts,
+      progress: percentage
+    }
+  });
+}
+
+exec({
+  hash: "ca340af3941fab7918933023e48efa72b1768e04",
+  fpath: "/home/everthis/downloads/Moana.2016.BluRay.720p.x264.DTS-HDChina.mp4"
+});
